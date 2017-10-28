8ea860c8089fba60cd51b0ca65a638f3bc00bb1f
Adrian Brandemuehl <abrandemuehl@gmail.com>
2017-10-28 13:14:45 -0500

    Add configuration based on rosparam values

--- src/webgui/src/webgui/server.py
+++ src/webgui/src/webgui/server.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+# This is needed in order to find templates, config and static directories due
+# to how catkin handles python file installation. Since it will `exec` this 
+# file from a different location, we must use the ROS API's to locate the real
+# instance_path
+import rospkg, sys, os
+rp = rospkg.RosPack()
+path = os.path.join(rp.get_path("webgui"), "src", "webgui")
+sys.path.insert(0, path)
+
 from flask import Flask, render_template
 import rospy
 from std_msgs.msg import String
-import os
-import sys
-import rospkg
 import threading
+import config
+
 
 
 class Sub:
@@ -13,14 +21,7 @@ class Sub:
         self.name = name
 
 sub = Sub('Enigma')
-
-# This is needed in order to find templates, config and static directories due
-# to how catkin handles python file installation. Since it will `exec` this 
-# file from a different location, we must use the ROS API's to locate the real
-# instance_path
-rp = rospkg.RosPack()
-app = Flask(__name__, instance_path=os.path.join(rp.get_path("webgui"), "src", "webgui"))
-
+app = Flask(__name__, instance_path=path)
 
 @app.route("/")
 def index():
@@ -32,7 +33,19 @@ def callback(data):
 
 
 def main():
-    if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+    # Check if we are doing an autonomous run. If so, the web gui should not be
+    # running as it is just a waste of resources
+    try:
+        mode = rospy.get_param('/mode')
+        if mode == 'autonomous':
+            # Don't run the gui on an autonomous run
+            print("Running in autonomous mode. Web GUI is not needed")
+            return
+    except KeyError:
+        pass
+
+    app.config.from_object('config.DevelopmentConfig')
+    if (not app.config['DEBUG']) or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
         # When running flask with debug=True, rospy has to be set up in this if
         # guard because the flask reloader forks and then calls the server, 
         # which will be killed and restarted on a reload. This causes some 
@@ -40,7 +53,7 @@ def main():
         # See https://stackoverflow.com/questions/25504149/why-does-running-the-flask-dev-server-run-itself-twice
         rospy.init_node('webserver', log_level=rospy.DEBUG,  disable_signals=True)
         rospy.Subscriber('testing', String, callback)
-        rospy.Subscriber("testing2", String, callback)
+
     # We do not need to run rospy.spin() here because all rospy.spin does is
     # block until the node is supposed to be killed
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
